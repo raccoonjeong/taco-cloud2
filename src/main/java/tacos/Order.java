5163c9727bea0d820dbe4502b6a36360890c19bc
raccoonjeong <rana3782@gmail.com>
2024-02-12 16:25:19 +0900

    118p

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -6,8 +6,16 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 @Data
 public class Order {
+
+    private Long id;
+    private Date createdAt;
+
     @NotBlank(message="Name is required")
     private String deliveryName;
 
@@ -30,4 +38,10 @@ public class Order {
     private String ccExpiration;
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
 }
